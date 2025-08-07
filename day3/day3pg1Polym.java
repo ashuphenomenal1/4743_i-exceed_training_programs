be03789b429a8dcec9d4564ed0cf8cfbcbb440a2
ashuphenomenal1 <ashutoshphenomenal1@gmail.com>
2025-08-07 15:57:56 +0530

    Create day3pg1Polym.java

---
+++ day3/day3pg1Polym.java
@@ -0,0 +1,11 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class day3pg1Polym {
+   public day3pg1Polym() {
+   }
+
+   public static void main(String[] var0) {
+      sample var2 = new sample();
+      new sample();
+      var2.display();
+   }
+}
