f1ef262fd01ed0add9eabac9c8c0f253eee1d31d
ashuphenomenal1 <ashutoshphenomenal1@gmail.com>
2025-08-07 15:51:49 +0530

    Add files via upload

---
+++ day4/day4pg2enccom.java
@@ -0,0 +1,47 @@
+class car{
+    private final engine eng;
+    String brand,model;
+    int year;
+    
+    car(String brand,String model,int year){
+        eng=new engine("Revatron","Third",4);
+        this.brand=brand;
+        this.model=model;
+        this.year=year;
+    }
+
+    public void displayCar(){
+        System.out.println("Brand of the car: "+brand);
+        System.out.println("Model of the car: "+model);
+        System.out.println("Year of the manufacturing: "+year);
+
+        eng.displayEngine();
+    }
+}
+
+    class engine{
+        String name,version;
+        int cylinder;
+         engine(String name,String version,int cylinder){
+            this.name=name;
+            this.version=version;
+            this.cylinder=cylinder;
+         }
+
+         public void displayEngine(){
+            System.out.println("Engine name: "+name);
+            System.out.println("Version of the engine: "+version);
+            System.out.println("Number of Cylinder: "+cylinder);
+         }
+
+    }
+
+public class day4pg2enccom {
+    
+public static void main(String[] args) {
+    car c=new car("Toyota","Fortuner Legender",2025);
+    c.displayCar();
+}
+
+
+}
---
+++ day4/day4pg3private.java
@@ -0,0 +1,26 @@
+ 
+import java.lang.reflect.Method;
+ 
+class sampledemo{
+  private String str="welcome";
+ 
+  private void displayPvt()
+  {
+    System.out.println("Accessing this method outside of the class");
+  }
+}
+ 
+class day4pg3private
+ {
+      public static void main(String[] args) throws Exception {
+        //   sampledemo obj=new sampledemo();
+        //   obj.displayPvt();
+          Class c=Class.forName("sampledemo");
+          Object o=c.newInstance();
+          Method m1= c.getDeclaredMethod("displayPvt");
+          m1.setAccessible(true);
+          m1.invoke(o);
+ 
+      } 
+ }
+ 
