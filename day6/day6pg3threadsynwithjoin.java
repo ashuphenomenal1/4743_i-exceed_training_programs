51556427368871ffd0f2bb71c55ac6857dc6ca1e
ashuphenomenal1 <ashutoshphenomenal1@gmail.com>
2025-08-11 12:36:59 +0530

    Add files via upload

---
+++ day6/day6pg3threadsynwithjoin.java
@@ -0,0 +1,44 @@
+class call{
+    synchronized public void callme(String msg){
+     System.out.print("[");
+     System.out.print(msg);
+     System.out.print("]");
+     display();
+    }
+    public void display(){
+        for(int i=0;i<10;i++){
+            System.out.print(i);
+        }
+    }
+}
+
+class caller extends Thread{
+    call obj;
+    String str;
+    Thread t=new Thread(this);
+
+    caller(call obj,String str){
+        this.obj=obj;
+        this.str=str;
+        t.start();
+    }
+
+        public void run(){
+           obj.callme(str);
+        }
+    }
+
+
+
+
+  class day6pg3threadsynwithjoin {
+    
+    public static void main(String[] args)  throws InterruptedException{
+        call obj=new call();
+        caller c1=new caller(obj,"Hello");
+        c1.join();
+        caller c2=new caller(obj,"Hii");
+        c2.join();
+        caller c3=new caller(obj,"Rao Sahab");
+    }
+}
