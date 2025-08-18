48f14591a4e3167b2a81a9aabead2816ad2a318e
ashuphenomenal1 <ashutoshphenomenal1@gmail.com>
2025-08-18 13:29:50 +0530

    Add files via upload

---
+++ JavaScript/day1/let.js
@@ -0,0 +1,18 @@
+function let()
+ {
+    let i=0;
+    for(let i=0;i<5;i++)
+    {
+        console.log(i);
+    }
+    //console.log(i);
+ }
+ let();
+ 
+
+ 
+
+//let x=1000;
+//let x=100;
+//console.log(x);
+ 
