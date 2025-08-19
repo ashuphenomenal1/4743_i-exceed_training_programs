6bd94a13f155129c02b03c866b97a5e20bd9d94f
ashuphenomenal1 <ashutoshphenomenal1@gmail.com>
2025-08-19 16:11:21 +0530

    Add files via upload

---
+++ JavaScript/day2/conversion.js
@@ -0,0 +1,28 @@
+let x="123";
+console.log(Number(x));
+console.log(typeof x);
+ 
+let y=null;
+console.log(y);
+console.log(typeof y);
+console.log(Number(y));
+ 
+let z=undefined;
+console.log(z);
+console.log(typeof z);
+console.log(Number(z));
+ 
+let a=false;
+console.log(typeof a);
+console.log(Number(a));
+ 
+let a1=true;
+console.log(typeof a1);
+console.log(Number(a1));
+ 
+let a2="hi";
+console.log(Number(a2));
+ 
+// Boolean of 0 ,null, undefined ,NaN resultant with false
+let res=Boolean(1);
+console.log(res)
