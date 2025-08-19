add476ce50f27df7f7da970b63f9b4be2c7ba593
ashuphenomenal1 <ashutoshphenomenal1@gmail.com>
2025-08-19 17:30:00 +0530

    Add files via upload

---
+++ JavaScript/day2/comparison.js
@@ -0,0 +1,13 @@
+console.log("A">"z")
+
+console.log("A">"Z")
+
+console.log("A"<"z")
+
+console.log("welcome to all" < "welcome")
+
+console.log("welcome"=="welcome")
+
+console.log("A">"B")
+
+console.log("welcome to all" > "welcome")
