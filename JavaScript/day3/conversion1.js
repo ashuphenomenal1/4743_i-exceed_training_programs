7e0004e7079aa14c12739e394a29771425453eba
ashuphenomenal1 <ashutoshphenomenal1@gmail.com>
2025-08-20 14:57:14 +0530

    Create conversion1.js

---
+++ JavaScript/day3/conversion1.js
@@ -0,0 +1,11 @@
+console.log(Number("123"));
+console.log(Number(true))
+console.log(Number(null))
+console.log(Number(undefined))
+
+console.log(Boolean(0))
+console.log(Boolean(NaN))
+console.log(Boolean(null))
+
+console.log(5!="5")
+console.log(5!=="5")
